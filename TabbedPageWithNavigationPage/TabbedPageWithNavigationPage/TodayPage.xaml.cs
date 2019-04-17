08b8b494f9efa0bb98455e817be1ac07e80cf400
XfHua <jasonHuaxf@163.com>
2019-04-17 14:47:03 +0800

    13
    123

---
+++ TabbedPageWithNavigationPage/TabbedPageWithNavigationPage/TodayPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class TodayPage : CarouselPage
+    {
+		public TodayPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
+
